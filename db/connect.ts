a4dc16aa642c0cb34527d119fc2adf7ff06b247b
Rustem Akhmetov <rustem.akhmetov@sonicdrivein.com>
2021-09-27 12:32:12 +0200

    connect to mongodb

--- db/connect.ts
+++ db/connect.ts
@@ -1,19 +1,22 @@
 import { Db, MongoClient } from 'mongodb';
 
-/**
- * We have to cache the DB connection
- * when used in a serverless environment otherwise
- * we may encounter performance loss due to
- * time to connect. Also, depending on your DB,
- * you might night be able to have many concurrent
- * DB connections. Most traditional DBs were not made for a stateless
- * environment like serverless. A serverless DB (HTTP based DB) whould work
- * better.
- */
 global.mongo = global.mongo || {};
 
 export const connectToDB = async () => {
-  const db = {};
+  if (!global.mongo) {
+    const client = new MongoClient(process.env.DATABASE_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      bufferMaxEntries: 0,
+      connectTimeoutMS: 10_000,
+    });
+
+    await global.mongo.client.connect();
+
+    global.mongo.client = client;
+  }
+
+  const db = global.mongo.client.db('known');
 
   return { db, dbClient: global.mongo.client };
 };
